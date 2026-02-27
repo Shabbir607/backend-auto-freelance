decbc49eb9d4cfe927aa5f3e6a553e325cb5ae14
Shabbir607 <ghulamshabbirandy607>
2026-02-27 13:38:03 +0500

    update api keys

--- config/longcat.php
+++ config/longcat.php
@@ -5,8 +5,6 @@ return [
     'model' => env('LONGCAT_MODEL', 'LongCat-Flash-Chat'),
     'api_keys' => [
         // Add your 100+ API keys here
-        'ak_2cZ7y73kj91D42m9XM5Cw0f10wf9C',
-        'ak_2Ca7198I71hS0cY9XW8Bc91S3PZ12',
         'ak_2lr7db3AO02f02l8d11s49sd0xB9V',
         'ak_2787TP3Vr08m02l4Dw64z5mI7Lr2q',
         'ak_2y77qc4nu7mC0aG54p77Y8mn9IK9A',
