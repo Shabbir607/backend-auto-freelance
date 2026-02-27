c35d6959d49f2fb638af2a8eed329af83401934d
Shabbir607 <ghulamshabbirandy607>
2026-02-27 13:19:07 +0500

    update api keys

--- config/longcat.php
+++ config/longcat.php
@@ -9,7 +9,19 @@ return [
         'ak_2Ca7198I71hS0cY9XW8Bc91S3PZ12',
         'ak_2lr7db3AO02f02l8d11s49sd0xB9V',
         'ak_2787TP3Vr08m02l4Dw64z5mI7Lr2q',
-        'ak_2y77qc4nu7mC0aG54p77Y8mn9IK9A'
+        'ak_2y77qc4nu7mC0aG54p77Y8mn9IK9A',
+        'ak_2n87L74EW0yF8Z15808eH3t89935w',
+        'ak_2Ey7355Id4kd7xb3Hb9BO9Mp1r84G',
+        'ak_2LS73c50L8Br0wk3Ey4Vr5yx6CS30',
+        'ak_2p67kL5Z20uR7yO3A75wz7yQ7Tl1h',
+        'ak_2sk7E65522V46TD3PB6Bm29g68a74',
+        'ak_2nm7UE63w2nv1KX8cR8QK4kx59k2D',
+        'ak_2YW7fp6fB6cC4xX4S02wh0rh6Jj2w',
+        'ak_2Kw7JD6bF7BV3LF82D9pf8jE8Wi9L',
+        'ak_2IF7Do6YS3nP7Tg38D9Ao3F212n7C',
+        'ak_2ds7m76MH2tb3gl1aS2lr5jR2N32E',
+        'ak_2uK7ED7OM6pj9du0XU3tG53m8C34Q',
+        'ak_2F27Qq7zE4r68gR6gN3ES4Ua6ao05'
        
         
     ],
