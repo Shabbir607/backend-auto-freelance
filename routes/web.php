95afa84d0a2e2c967613a90447b2f431d64b7def
Shabbir607 <ghulamshabbirandy607@gmail.com>
2026-03-06 06:27:23 -0800

    fixed 404 error

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('/', function () {
 
 // Fallback for non-API routes
 Route::fallback(function () {
-    return response()->json(['message' => 'Not Found'], 404);
+    return redirect('https://edgelancer.com/', 301);
 });
+;
