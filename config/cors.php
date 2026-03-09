8e4dada7938ccf5490a110c992d8810bc5c1fbaa
Cyber Panel <marlenedewees607@gmail.com>
2026-03-09 20:09:51 +0000

    update ip rate

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:5173',
-        'http://localhost:5174',
+        'http://127.0.0.1:45678',
         'http://localhost:8000',
         'http://127.0.0.1:8000',
         'https://edgelancer.com',
