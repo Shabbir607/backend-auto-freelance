14c16c5dbc1d05faabdabcfd2bca5a3777846d29
Shabbir607 <ghulamshabbirandy607@gmail.com>
2026-03-10 03:30:34 -0700

    update number

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
 
             // 1. Check if the IP is persistently blocked (long-term)
             if (\Illuminate\Support\Facades\Cache::get("permanently_blocked_{$ip}")) {
-                return Limit::perMinute(111160)->by($ip)->response(function () {
+                return Limit::perMinute(60)->by($ip)->response(function () {
                     return response()->json([
                         'message' => 'Your IP has been flagged for suspicious activity and is temporarily blocked.'
                     ], 403);
@@ -36,18 +36,27 @@ class AppServiceProvider extends ServiceProvider
             }
 
             if (! $user) {
-                // Persistent limits for guests (usually scrapers)
-                return [
-                    // If they hit 10/min, we block them for 7 days
-                    Limit::perMinute(1111160)->by($ip)->response(function () use ($ip) {
-                        \Illuminate\Support\Facades\Cache::put("permanently_blocked_{$ip}", true, now()->addMinutes(10));
+
+            $ip = request()->ip();
+            $endpoint = request()->path(); // or request()->route()->getName()
+
+            return [
+                Limit::perMinute(60)
+                    ->by($ip . '|' . $endpoint)
+                    ->response(function () use ($ip) {
+
+                        \Illuminate\Support\Facades\Cache::put(
+                            "permanently_blocked_{$ip}",
+                            true,
+                            now()->addMinutes(10)
+                        );
+
                         return response()->json([
                             'message' => 'Rate limit exceeded. Your IP has been blocked'
                         ], 403);
                     }),
-                   
-                ];
-            }
+            ];
+        }
 
             // Strict limits for authenticated users
             return [
