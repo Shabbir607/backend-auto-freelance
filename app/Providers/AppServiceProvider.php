c6a0ac2a4dca9d0362137ef1a6be6a2d21f0183b
Shabbir607 <ghulamshabbirandy607@gmail.com>
2026-03-10 01:52:50 -0700

    update number

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
 
             // 1. Check if the IP is persistently blocked (long-term)
             if (\Illuminate\Support\Facades\Cache::get("permanently_blocked_{$ip}")) {
-                return Limit::perMinute(1160)->by($ip)->response(function () {
+                return Limit::perMinute(111160)->by($ip)->response(function () {
                     return response()->json([
                         'message' => 'Your IP has been flagged for suspicious activity and is temporarily blocked.'
                     ], 403);
@@ -39,7 +39,7 @@ class AppServiceProvider extends ServiceProvider
                 // Persistent limits for guests (usually scrapers)
                 return [
                     // If they hit 10/min, we block them for 7 days
-                    Limit::perMinute(1160)->by($ip)->response(function () use ($ip) {
+                    Limit::perMinute(1111160)->by($ip)->response(function () use ($ip) {
                         \Illuminate\Support\Facades\Cache::put("permanently_blocked_{$ip}", true, now()->addMinutes(10));
                         return response()->json([
                             'message' => 'Rate limit exceeded. Your IP has been blocked'
